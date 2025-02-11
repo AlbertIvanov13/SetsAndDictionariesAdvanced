52570f0c0e4ef8f89f71d325307f69e731c02937
Albert Ivanov <albertivanov187@gmail.com>
2025-02-11 22:59:50 +0200

    100/100

---
+++ 07. ParkingLot/Program.cs
@@ -0,0 +1,34 @@
+﻿
+HashSet<string> parking = new HashSet<string>();
+
+string input = Console.ReadLine();
+
+while (input != "END")
+{
+    string[] arguments = input.Split(", ");
+    string direction = arguments[0];
+    string carNumber = arguments[1];
+
+    if (direction == "IN")
+    {
+        parking.Add(carNumber);
+    }
+    else
+    {
+        parking.Remove(carNumber);
+    }
+
+    input = Console.ReadLine();
+}
+
+if (parking.Count > 0)
+{
+    foreach (string car in parking)
+    {
+        Console.WriteLine(car);
+    }
+}
+else
+{
+    Console.WriteLine("Parking Lot is Empty");
+}
