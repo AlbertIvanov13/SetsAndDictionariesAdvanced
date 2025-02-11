1ea6a77b299bc5448bb3350c7b93c12b55fcb58c
Albert Ivanov <albertivanov187@gmail.com>
2025-02-11 22:45:07 +0200

    100/100

---
+++ 05. CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,35 @@
+﻿
+int continentsCount = int.Parse(Console.ReadLine());
+
+Dictionary<string, Dictionary<string, List<string>>> continents = new Dictionary<string, Dictionary<string, List<string>>>();
+
+for (int i = 0; i < continentsCount; i++)
+{
+    string[] input = Console.ReadLine().Split();
+    string continent = input[0];
+    string country = input[1];
+    string city = input[2];
+
+    if (!continents.ContainsKey(continent))
+    {
+        continents.Add(continent, new Dictionary<string, List<string>>());
+    }
+
+    if (!continents[continent].ContainsKey(country))
+    {
+        continents[continent].Add(country, new List<string>());
+    }
+
+    continents[continent][country].Add(city);
+}
+
+
+foreach (var continent in continents)
+{
+    Console.WriteLine($"{continent.Key}:");
+    foreach (var (country, cities) in continent.Value)
+    {
+        Console.Write($"    {country} -> ");
+        Console.WriteLine(string.Join(", ", cities));
+    }
+}
