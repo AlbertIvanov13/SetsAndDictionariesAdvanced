8b847e1532f7f79ea25dedd5094950e088b85624
Albert Ivanov <albertivanov187@gmail.com>
2025-02-11 17:52:28 +0200

    100/100

---
+++ 03. Largest3Numbers/Program.cs
@@ -0,0 +1,6 @@
+﻿
+int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+numbers = numbers.OrderByDescending(x => x).Take(3).ToArray();
+
+Console.WriteLine(string.Join(" ", numbers));
