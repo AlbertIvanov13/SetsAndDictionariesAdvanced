95e841b1316b10404a3a2edea43c892d4efd44bf
Albert Ivanov <albertivanov187@gmail.com>
2025-02-11 17:32:02 +0200

    100/100

--- 01. CountSameValuesInArray/Program.cs
+++ 01. CountSameValuesInArray/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+double[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+Dictionary<double, int> dictionary = new Dictionary<double, int>();
+
+int counter = 0;
+foreach (double number in numbers)
+{
+	if (!dictionary.ContainsKey(number))
+	{
+		dictionary.Add(number, counter);
+	}
+
+	dictionary[number]++;
+}
+
+foreach (var (number, times) in dictionary)
+{
+    Console.WriteLine($"{number} - {times} times");
+}
