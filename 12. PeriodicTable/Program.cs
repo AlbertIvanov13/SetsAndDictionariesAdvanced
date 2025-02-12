723f4543a8b70c919f9030034aac5815a7220931
Albert Ivanov <albertivanov187@gmail.com>
2025-02-12 21:26:13 +0200

    100/100

---
+++ 12. PeriodicTable/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int number = int.Parse(Console.ReadLine());
+
+HashSet<string> chemicalElements = new HashSet<string>();
+
+for (int i = 0; i < number; i++)
+{
+    string[] elements = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    foreach (string element in elements)
+    {
+        chemicalElements.Add(element);
+    }
+}
+
+Console.WriteLine(string.Join(" ", chemicalElements.OrderBy(x => x)));
