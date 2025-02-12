7072214772d994b0c87b57d73fecc1fc94985b41
Albert Ivanov <albertivanov187@gmail.com>
2025-02-12 21:10:49 +0200

    100/100

---
+++ 10. UniqueUsernames/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int number = int.Parse(Console.ReadLine());
+
+HashSet<string> usernames = new HashSet<string>();
+
+for (int i = 0; i < number; i++)
+{
+    string username = Console.ReadLine();
+
+    usernames.Add(username);
+}
+
+foreach (string username in usernames)
+{
+    Console.WriteLine(username);
+}
