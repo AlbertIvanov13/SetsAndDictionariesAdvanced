b38594892aac352571d36ce0c163698263471b25
Albert Ivanov <albertivanov187@gmail.com>
2025-02-12 22:02:52 +0200

    100/100

---
+++ 14. CountSymbols/Program.cs
@@ -0,0 +1,19 @@
+﻿
+string text = Console.ReadLine();
+
+Dictionary<char, int> occurrences = new Dictionary<char, int>();
+
+foreach (var item in text)
+{
+	if (!occurrences.ContainsKey(item))
+	{
+		occurrences[item] = 0;
+	}
+
+	occurrences[item]++;
+}
+
+foreach (var (symbol, times) in occurrences.OrderBy(x => x.Key))
+{
+	Console.WriteLine($"{symbol}: {times} time/s");
+}
